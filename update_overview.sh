5243378253343f2f43fe83859d3b6124bcecd48f
Odin Hørthe Omdal <odin.omdal@gmail.com>
2016-07-04 13:18:23 +0200

    Make update_overview.sh stderr-silent

--- update_overview.sh
+++ update_overview.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
+git checkout master 2> /dev/null
+
 for p in chromium.blink chromium.src v8; do
-    git --git-dir=$p.git fetch
+    git --git-dir=$p.git fetch 2> /dev/null
 done
 
 python generate_upstream_list.py > upstream.html
-git checkout gh-pages
+git checkout gh-pages 2> /dev/null
 mv upstream.html index.html
 git commit -m "Update upstream overview." index.html
